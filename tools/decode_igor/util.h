cf3c577c1ce8b27ba9ed8a391c58e66986545c16
Gregory Montoir <cyx@users.sourceforge.net>
2016-08-23 07:15:53 +0800

    decode_igor: remove unused READ_REAL

--- tools/decode_igor/util.h
+++ tools/decode_igor/util.h
@@ -26,19 +26,4 @@ static inline void WRITE_LE_UINT32(uint8_t *b, uint32_t value) {
 	WRITE_LE_UINT16(&b[2], value >> 16);
 }
 
-static inline double READ_REAL(uint16_t a, uint16_t b, uint16_t c) {
-	if ((a & 0xFF) == 0) {
-		return 0.;
-	}
-	const int sign = ((c & 0x8000) != 0) ? -1 : 1;
-	const int mantissaBits = ((c & 0x7FFF) << 24) | (b << 8) | (a >> 8);
-	double mantissa = 1.;
-	for (int i = 0; i < 5; ++i) {
-		const int n = (mantissaBits >> (8 * (4 - i))) & 0xFF;
-		mantissa += n / (float)(1 << (7 + i * 8));
-	}
-	const int exponent = (a & 0xFF) - 129;
-	return sign * (1 << exponent) * mantissa;
-}
-
 #endif
